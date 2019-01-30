2211a47bf83fb79a8ea2c9b39a173a6632ff484b
smuruga5 <smuruga5@asu.edu>
2019-01-30 02:34:41 -0700

    Update 2

--- main.cpp
+++ main.cpp
@@ -1,32 +1,41 @@
+/*
+----------------------------------------------------------------------------------
+Author Name: Sujjanaraj Muruganandam
+Project    : CSE 330 Project1 Milestone
+----------------------------------------------------------------------------------
+*/
+
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main(int argc, char * argv[])
 {
-    ifstream file;
-    string line;
-    int count;
-    int offset;
+    ifstream file;//Creating a File
+    string line;//String to read the input from File Line by Line
+    int count;//Counter to keep track of Line
+    int offset;//Variable to find the Word and its position
+    int size=(sizeof argv[1]/8)-1;//Size of Word to be searched
     file.open(argv[2]);
+
     while(file)
     {
+    getline(file,line);//Function to get Line
 
-    getline(file,line);
     count++;
-    if((offset=line.find(argv[1],0))!=string::npos)
+
+    offset=line.find(argv[1],0);//Find function that returns the position of the word.
+
+    int tell=file.tellg();//tellg function to get the total length of the Line
+
+    int pos=offset;//Position Variable
+
+    if((offset)!=string::npos)
     {
-       cout<< argv[2]<<", "<<file.tellg()<<", "<<count<<": "<<argv[1]<<endl;
+       cout<< argv[2]<<", "<<count<<", "<<offset<<", "<< (offset+size)<<": "<<argv[1]<<endl; //Displaying in the required Output Format
+
+       pos=tell-offset; //To reset the Position
     }
-    //cout<<line<<endl;
-
     }
-
-    cout << "You have entered " << argc
-         << " arguments:" << "\n";
-
-    for (int i = 0; i < argc; ++i)
-        cout << argv[i] << "\n";
-
     return 0;
 }
