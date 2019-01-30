c19d4b9e81674fff27db23dae6c00c7329ee6c61
smuruga5 <smuruga5@asu.edu>
2019-01-29 23:48:51 -0700

    Update 1

--- main.cpp
+++ main.cpp
@@ -1,59 +1,32 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
-public:
-    void search()
-    {
-
-    }
-int main(int argc, string argv[])
+int main(int argc, char * argv[])
 {
-    fstream file;//Creating a file
-    file.open(argv[2]);//Opening the file
-
-    //Check for correct input format in the Command Line
-    if(argc!=3)
+    ifstream file;
+    string line;
+    int count;
+    int offset;
+    file.open(argv[2]);
+    while(file)
     {
-        cout<<"Usage: search.exe <text> <filename>\n";
-        return -1;
+
+    getline(file,line);
+    count++;
+    if((offset=line.find(argv[1],0))!=string::npos)
+    {
+       cout<< argv[2]<<", "<<file.tellg()<<", "<<count<<": "<<argv[1]<<endl;
     }
-
-    //Check is the file contains anything
-    if(!file)
-    {
-        cout<<"Failed to open file";
-        return 0;
-    }
-
-    string sent;//String for storing the line
-    string sent2;//String for storing the second string if Union is present
-    int count=1;//Variable to keep track of the line count
-
-    while(getline(file, sent))
-    {
-        for(int i=0;i<sent.length();i++)
-        {
-            if(sent.at(i)==argv[1].at(0))
-            {
-
-                for(int j=0;j<argv[1].length();j++}
-                {
-                if(argv[1].at(j)=="+")
-                {
-                  sent2=argv[1].substr(j+1,argv[1].length()-1);
-
-                }
-                if(sent.at(j)==argv[1].at(j))
-                {
-                    cout<<argv[2]<<", "<<count<<", "<<i<<", "<<j<<": "<<argv[1]<<endl;
-                }
-                }
-            }
-        }
+    //cout<<line<<endl;
 
     }
 
+    cout << "You have entered " << argc
+         << " arguments:" << "\n";
 
+    for (int i = 0; i < argc; ++i)
+        cout << argv[i] << "\n";
 
+    return 0;
 }
